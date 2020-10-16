b335c7be265cf25031f758fe1f08a4b918d8967f
sriram gn <38837878+Sriramgn0@users.noreply.github.com>
2020-10-16 10:14:06 +0530

    Update Factorial.c
    check for negative numbers and display message

--- c/Factorial.c
+++ c/Factorial.c
@@ -3,15 +3,19 @@ using namespace std;
 int main()
 {
   int i,fac=1,num;
-  cout<<"Enter number of which u want to calculate factorial";
+  cout<<"Enter number of which you want to calculate factorial";
   cin>>num;
-
-    for(i=1;i<=num;i++)
+    if(num>=0)
     {
-      fac=fac*i;
-    }
-
+       for(i=1;i<=num;i++)
+      {
+        fac=fac*i;
+      }
    cout<<"factorial of " <<num<< "is:" <<fac<<endl;
+    }
+    else
+    {
+     cout<<"can't find factorial for negative numbers";
 
   return 0;
 }
